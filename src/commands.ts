cf8aab3de0cca840dab3a43667ad131670a17a61
Mouin Boubakri <MohamedMouin.Boubakri@Clarrio.ai>
2026-08-30 08:57:05 +0100

    feat: implement user management commands and database integration

--- src/commands.ts
+++ src/commands.ts
@@ -1,26 +1,53 @@
 import { setUser } from "./config";
+import { createUser, deleteUsers, getUserByName } from "./lib/db/queries/users";
 
-export type CommandHandler = (cmdName: string, ...args: string[]) => void;
+export type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
 export type CommandsRegistry = { [key: string]: CommandHandler };
 
 
-export function handlerLogin(cmdName: string, ...args: string[]):void {
+export async function handlerLogin(cmdName: string, ...args: string[]): Promise<void> {
     if (args.length < 1) {
         throw new Error("Username is required for login command");
+    }
+    if(!await getUserByName(args[0])) {
+        throw new Error("User does not exist");
     }
     setUser(args[0]);
     console.log('User has been set');
 }
-export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler):void{
+export async function handlerRegister(cmdName: string, ...args: string[]): Promise<void> {
+    if (args.length < 1) {
+        throw new Error("Username is required for register command");
+    }
+    if(await getUserByName(args[0])) {
+        throw new Error("User already exists");
+    }
+    const user = await createUser(args[0]);
+    if (!user) {
+        throw new Error("Failed to create user");
+    }
+    else{
+        console.log('User has been created:', await getUserByName(user.name));
+        setUser(user.name); 
+    }
+    
+}
+
+export async function handlerDeleteUsers(cmdName:string) : Promise<void> {
+    const result = await deleteUsers();
+    return result ? console.log("All users have been deleted") : console.log("Failed to delete users");
+}
+
+export async function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler): Promise<void> {
     registry[cmdName] = handler;
 }
 
-export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]):void {
+export async function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]): Promise<void> {
     const handler = registry[cmdName];
     if (!handler) {
         throw new Error(`Command "${cmdName}" not found`);
     }
-    handler(cmdName, ...args);
+    await handler(cmdName, ...args);
 }
     
     
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { handlerLogin, registerCommand, runCommand, CommandsRegistry } from "./commands";
+import { handlerLogin, registerCommand, runCommand, CommandsRegistry, handlerRegister, handlerDeleteUsers } from "./commands";
 
-function main() {
+async function main() {
 const commandRegistry: CommandsRegistry = {};
 
 registerCommand(commandRegistry, "login", handlerLogin);
+registerCommand(commandRegistry, "register", handlerRegister)
+registerCommand(commandRegistry, "reset", handlerDeleteUsers)
 const cliArgs = process.argv.slice(2);
 if (cliArgs.length < 1) {
   console.log("No command provided");
@@ -11,12 +13,13 @@ if (cliArgs.length < 1) {
 }
  const [first, ...remaining] = cliArgs;
  try {
- runCommand(commandRegistry, first, ...remaining);
+await runCommand(commandRegistry, first, ...remaining);
 
 } catch (error) {
   console.error("Error occurred while running command:", error);
   process.exit(1);
 }
+process.exit(0);
 }
 main();
 
